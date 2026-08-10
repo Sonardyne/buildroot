9c02bb9f324c3d13ff796f6182e715905a42b8ea
jle@snet.sonardyne.net <josh.etherington@sonardyne.com>
2026-08-10 11:20:28 +0100

    Import updated python-protobug from pypi

--- package/python-protobuf/python-protobuf.mk
+++ package/python-protobuf/python-protobuf.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PROTOBUF_VERSION = $(PROTOBUF_VERSION)
-PYTHON_PROTOBUF_SOURCE = protobuf-python-4.$(PYTHON_PROTOBUF_VERSION).tar.gz
-PYTHON_PROTOBUF_SITE = $(PROTOBUF_SITE)
+# Fetched directly from PyPI rather than sharing PROTOBUF_VERSION/PROTOBUF_SITE
+PYTHON_PROTOBUF_VERSION = 6.33.5
+PYTHON_PROTOBUF_SOURCE = protobuf-$(PYTHON_PROTOBUF_VERSION).tar.gz
+PYTHON_PROTOBUF_SITE = https://files.pythonhosted.org/packages/ba/25/7c72c307aafc96fa87062aa6291d9f7c94836e43214d43722e86037aac02
 PYTHON_PROTOBUF_LICENSE = BSD-3-Clause
 PYTHON_PROTOBUF_LICENSE_FILES = LICENSE
-PYTHON_PROTOBUF_DEPENDENCIES = host-protobuf
 PYTHON_PROTOBUF_SETUP_TYPE = setuptools
-PYTHON_PROTOBUF_SUBDIR = python
 
 $(eval $(python-package))
