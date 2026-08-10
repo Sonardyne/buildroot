02269d6b51caf1e8f78c4ff71ee650c39df4f9d5
jle@snet.sonardyne.net <josh.etherington@sonardyne.com>
2026-08-10 15:10:57 +0100

    Import updated python-grpcio from pypi

--- package/python-grpcio/python-grpcio.mk
+++ package/python-grpcio/python-grpcio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GRPCIO_VERSION = 1.60.0
+PYTHON_GRPCIO_VERSION = 1.81.0
 PYTHON_GRPCIO_SOURCE = grpcio-$(PYTHON_GRPCIO_VERSION).tar.gz
-PYTHON_GRPCIO_SITE = https://files.pythonhosted.org/packages/61/38/c615b5c2be690fb31871f294cc08a96e598b085b8d07c5967a5018e0b90c
+PYTHON_GRPCIO_SITE = https://files.pythonhosted.org/packages/15/f3/23f47b24f8d8c2028eba501db3acfbb2f592cbb5995eaa6e363a627b74d7
 PYTHON_GRPCIO_SETUP_TYPE = setuptools
 PYTHON_GRPCIO_LICENSE = Apache-2.0
 PYTHON_GRPCIO_LICENSE_FILES = LICENSE
